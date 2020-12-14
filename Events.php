a84891a07c060d09bec291ad1701f726a597ee81
Lucas Bartholemy <lucas@bartholemy.com>
2020-12-14 12:01:30 +0100

    Use new menu API

--- Events.php
+++ Events.php
@@ -2,8 +2,12 @@
 
 namespace acmeCorp\humhub\modules;
 
+use humhub\modules\admin\permissions\ManageModules;
+use humhub\modules\admin\widgets\AdminMenu;
+use humhub\modules\ui\menu\MenuLink;
+use humhub\widgets\TopMenu;
 use Yii;
-use yii\helpers\Url;
+use yii\base\Event;
 
 class Events
 {
@@ -14,29 +18,35 @@ class Events
      */
     public static function onTopMenuInit($event)
     {
-        $event->sender->addItem([
-            'label' => 'Example-basic',
-            'icon' => '<i class="fa fa-anchor"></i>',
-            'url' => Url::to(['/example-basic/index']),
+        /** @var TopMenu $topMenuWidget */
+        $topMenuWidget = $event->sender;
+
+        $topMenuWidget->addEntry(new MenuLink([
+            'label' => Yii::t('ExampleBasicModule.base', 'Example'),
+            'icon' => 'anchor',
+            'url' => ['/example-basic/index'],
             'sortOrder' => 99999,
             'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'example-basic' && Yii::$app->controller->id == 'index'),
-        ]);
+        ]));
     }
 
     /**
      * Defines what to do if admin menu is initialized.
      *
-     * @param $event
+     * @param $event Event
      */
     public static function onAdminMenuInit($event)
     {
-        $event->sender->addItem([
-            'label' => 'Example-basic',
-            'url' => Url::to(['/example-basic/admin']),
-            'group' => 'manage',
-            'icon' => '<i class="fa fa-anchor"></i>',
+        /** @var AdminMenu $adminMenuWidget */
+        $adminMenuWidget = $event->sender;
+
+        $adminMenuWidget->addEntry(new MenuLink([
+            'label' => Yii::t('ExampleBasicModule.base', 'Example'),
+            'url' => ['/example-basic/admin'],
+            'icon' => 'info-circle',
+            'sortOrder' => 1000,
             'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'example-basic' && Yii::$app->controller->id == 'admin'),
-            'sortOrder' => 99999,
-        ]);
+            'isVisible' => Yii::$app->user->can(ManageModules::class)
+        ]));
     }
 }
